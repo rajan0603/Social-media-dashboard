902dbac3a8a6a13864244e71a733522013460c22
rajan <rajansuhagiya1234g@gmail.com>
2024-02-07 19:03:08 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
  
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://social-media-dashboard-backend.vercel.app/"],
+        methods: ["POST","GET"],
+        credentials: true
+    }
+));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
  
 const storage = multer.diskStorage({
