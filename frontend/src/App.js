e648fa9959a92c44509e6f82d72267f49166bb70
rajan <rajansuhagiya1234g@gmail.com>
2024-02-07 18:44:37 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import CreatePost from './components/CreatePost';
+import './App.css';
+
+function App() {
+	return (
+		<Router>
+			<div className="app">
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/create">Create Post</Link>
+						</li>
+					</ul>
+				</nav>
+				<Routes>
+					<Route path="/create" element={<CreatePost />} />
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</div>
+		</Router>
+	);
+}
+
+export default App;
+
