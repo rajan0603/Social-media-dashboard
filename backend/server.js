503840345c1b7cf1ba4d165681b400915c13be31
rajan <rajansuhagiya1234g@gmail.com>
2024-02-07 20:54:50 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
  
 app.use(cors(
     {
-        origin: [""],
+        origin: ["https://social-media-dashboard-frontend.vercel.app"],
         methods: ["POST","GET"],
         credentials: true
     }
